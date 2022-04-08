3218b490e3a4fe37f56f62232e23a33fb169581f
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-08 22:12:14 +0200

    reward

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import { useState } from "react";
 import TinderCard from "react-tinder-card";
 import splash from "./splash.json";
 import "./App.css";
+import { useReward } from "react-rewards";
+import { useDoubleTap } from "use-double-tap";
 
 function App() {
     const images = Array.from(new Map(Object.entries(splash.data)).values());
     const [margin, setMargin] = useState(10);
+    const { reward } = useReward("rewardId", "emoji");
 
     const onSwipe = () => {
         setMargin(margin + 1);
     };
 
+    const bind = useDoubleTap((event) => {
+        reward();
+    });
+
     return (
         <div>
             <div className="cardContainer">
                 {images.slice(0, margin).map((image, no) => (
-                    <TinderCard onSwipe={onSwipe} key={no} className="swipe">
+                    <TinderCard id="rewardId" {...bind} onSwipe={onSwipe} key={no} className="swipe">
                         <div style={{ backgroundImage: "url(" + image.photoUrl + ")" }} className="card"></div>
                     </TinderCard>
                 ))}
