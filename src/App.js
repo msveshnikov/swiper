2f0cc3d80d448ff375b561d154487b261ed080a9
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-08 21:24:14 +0200

    final fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import TinderCard from "react-tinder-card";
 import splash from "./splash.json";
 import "./App.css";
 
 function App() {
     const map = new Map(Object.entries(splash.data));
-    const images = Array.from(map.values()).slice(0, 50);
+    const images = Array.from(map.values());
+    const [margin, setMargin] = useState(10);
+
+    const onSwipe = () => {
+        setMargin(margin + 1);
+    };
+
     return (
         <div>
             <div className="cardContainer">
-                {Array.from(images).map((image, no) => (
-                    <TinderCard key={no} className="swipe">
+                {Array.from(images.slice(0, margin)).map((image, no) => (
+                    <TinderCard onSwipe={onSwipe} key={no} className="swipe">
                         <div style={{ backgroundImage: "url(" + image.photoUrl + ")" }} className="card"></div>
                     </TinderCard>
                 ))}
