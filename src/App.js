f9a45c75b20e0c14def926de8165ff14d9a2db9e
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-08 22:02:31 +0200

    refac

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import splash from "./splash.json";
 import "./App.css";
 
 function App() {
-    const map = new Map(Object.entries(splash.data));
-    const images = Array.from(map.values());
+    const images = Array.from(new Map(Object.entries(splash.data)).values());
     const [margin, setMargin] = useState(10);
 
     const onSwipe = () => {
@@ -16,7 +15,7 @@ function App() {
     return (
         <div>
             <div className="cardContainer">
-                {Array.from(images.slice(0, margin)).map((image, no) => (
+                {images.slice(0, margin).map((image, no) => (
                     <TinderCard onSwipe={onSwipe} key={no} className="swipe">
                         <div style={{ backgroundImage: "url(" + image.photoUrl + ")" }} className="card"></div>
                     </TinderCard>
