e21ebf1e67078e2a6e0f9057c4854750eeeac07c
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-09 02:50:27 +0200

    minor

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useState } from "react";
 import TinderCard from "react-tinder-card";
 import splash from "./splash.json";
-import "./App.css";
-import _ from "underscore";
 import { useReward } from "react-rewards";
 import { useDoubleTap } from "use-double-tap";
+import _ from "underscore";
+import "./App.css";
 
 const images = _.shuffle(
     Array.from(new Map(Object.entries(splash.data)).values()).filter((image) => image?.createdAt?.value)
@@ -19,9 +19,9 @@ function App() {
         setMargin((prev) => prev + 1);
     };
 
-    const bind = useDoubleTap((event) => {
+    const bind = useDoubleTap(() => {
         reward();
-    });
+    }, 500);
 
     return (
         <div>
@@ -30,12 +30,8 @@ function App() {
                     .slice(margin - 10, margin)
                     .reverse()
                     .map((image) => (
-                        <TinderCard onSwipe={onSwipe} key={image?.createdAt?.value} className="swipe">
-                            <div
-                                {...bind}
-                                style={{ backgroundImage: "url(" + image.photoUrl + ")" }}
-                                className="card"
-                            ></div>
+                        <TinderCard {...bind} onSwipe={onSwipe} key={image?.createdAt?.value} className="swipe">
+                            <div {...bind} style={{ backgroundImage: "url(" + image.photoUrl + ")" }} className="card"></div>
                             <span id="rewardId" />
                         </TinderCard>
                     ))}
