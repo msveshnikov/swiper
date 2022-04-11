5d45e13d288bb609f9d0cf574e2b558187ce8290
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-11 08:58:20 +0200

    clen

--- src/Heart.js
+++ src/Heart.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useState } from "react";
-import { useReward } from "react-rewards";
 import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
@@ -20,8 +18,6 @@ const useStyles = makeStyles(() => ({
 
 const Heart = ({liked, setLiked, reward}) => {
     const classes = useStyles();
-
-    // const { reward } = useReward("rewardId", "emoji", { zIndex: 10 });
 
     const handleLikeClick = () => {
         if (!liked) {
