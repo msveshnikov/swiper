02056c76c8b73231e58ea88dccc159dcc64e53e0
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-13 22:01:58 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import * as Sentry from "@sentry/react";
@@ -9,18 +8,11 @@ import { createRoot } from "react-dom/client";
 Sentry.init({
     dsn: "https://505a033cd95c41eda67fac2ce3e9a9aa@o383246.ingest.sentry.io/6324817",
     integrations: [new BrowserTracing()],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.2,
 });
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
