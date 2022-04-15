7deb9e591dd82209dd3ce288c2bedbd432b91518
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-15 10:18:13 +0200

    release sentry

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserTracing } from "@sentry/tracing";
 import { createRoot } from "react-dom/client";
 
 Sentry.init({
+    release: "swiper@" + process.env.npm_package_version,
     dsn: "https://505a033cd95c41eda67fac2ce3e9a9aa@o383246.ingest.sentry.io/6324817",
     integrations: [new BrowserTracing()],
     tracesSampleRate: 0.2,
