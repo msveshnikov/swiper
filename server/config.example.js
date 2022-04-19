75ba18366ecf2323f20b0a8d367686dec9755d17
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-19 08:33:49 +0200

    origin

--- server/config.example.js
+++ server/config.example.js
@@ -1,3 +1,3 @@
-export const ALLOWED_ORIGIN = "http://localhost:3000";
+export const ALLOWED_ORIGIN = ["https://swiper.maxsoft.tk", "https://swiper.ml", "http://localhost:3000"];
 export const MONGODB_URI =
     "mongodb+srv://[user]:[password]@cluster0.f7292.mongodb.net/[database]?retryWrites=true&w=majority";
