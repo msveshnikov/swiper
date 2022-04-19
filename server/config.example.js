82f9c3d6e29f011386481d252655b960d7777088
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-19 08:39:08 +0200

    update

--- server/config.example.js
+++ server/config.example.js
@@ -1,3 +1,8 @@
-export const ALLOWED_ORIGIN = ["https://swiper.maxsoft.tk", "https://swiper.ml", "http://localhost:3000"];
+export const ALLOWED_ORIGIN = [
+    "https://swipe.maxsoft.tk",
+    "https://swiper.ml",
+    "https://develop--photo-swiper.netlify.app",
+    "http://localhost:3000",
+];
 export const MONGODB_URI =
     "mongodb+srv://[user]:[password]@cluster0.f7292.mongodb.net/[database]?retryWrites=true&w=majority";
