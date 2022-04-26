fbbfecfc28bb3ca7a837228a3ef5a71f6d739abd
Max Sveshnikov <msveshnikov@gmail.com>
2022-04-26 09:15:08 +0200

    palette

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import Layout from "./Layout/Layout";
 import Cards from "./Components/Cards";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import MyLikes from "./Components/MyLikes";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+    palette: {
+        type: "dark",
+        background: {
+            paper: "#e66465",
+        },
+        primary: {
+            main: "#9198e5",
+        },
+        secondary: {
+            main: "#9198e5",
+        },
+    },
+});
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path="/likes" element={<MyLikes />} />
-                    <Route path="/" element={<Cards />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="/likes" element={<MyLikes />} />
+                        <Route path="/" element={<Cards />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 };
 
